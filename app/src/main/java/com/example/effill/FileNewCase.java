83e17f681af32efe1ccc761dcce8c15cbb34dd5a
Dennis525 <dennisokumu72@yahoo.com>
2022-03-01 14:33:48 +0300

    first android studio app e-filing

--- app/src/main/java/com/example/effill/FileNewCase.java
+++ app/src/main/java/com/example/effill/FileNewCase.java
@@ -2,10 +2,16 @@ package com.example.effill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class FileNewCase extends AppCompatActivity {
 
@@ -21,5 +27,29 @@ public class FileNewCase extends AppCompatActivity {
 
         myArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpin.setAdapter(myArrayAdapter);
+        mySpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+              String itemSelected = mySpin.getSelectedItem().toString();
+                Toast.makeText(FileNewCase.this, "You have selected "  + itemSelected, Toast.LENGTH_SHORT).show();
+                 if (i == 1){
+                    String itemSelected1 = mySpin.getSelectedItem().toString();
+                    ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(FileNewCase.this,android.R.layout.simple_list_item_1,
+                            getResources().getStringArray(R.array.supreme_court));
+                    myArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    mySpin.setAdapter(myArrayAdapter);
+                    Toast.makeText(FileNewCase.this, "You have selected " + itemSelected1,  Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
