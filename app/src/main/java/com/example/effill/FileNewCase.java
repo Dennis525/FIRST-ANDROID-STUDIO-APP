1ce0f36b6048228d4c2283e61fcfbe04854efec8
Dennis525 <dennisokumu72@yahoo.com>
2022-03-01 10:02:06 +0300

    first android studio app e-filing

--- app/src/main/java/com/example/effill/FileNewCase.java
+++ app/src/main/java/com/example/effill/FileNewCase.java
@@ -3,12 +3,23 @@ package com.example.effill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FileNewCase extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_file_new_case);
+
+        Spinner mySpin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(FileNewCase.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.courts));
+
+        myArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpin.setAdapter(myArrayAdapter);
     }
 }
